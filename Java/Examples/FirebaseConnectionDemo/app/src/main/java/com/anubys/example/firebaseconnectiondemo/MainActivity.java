f81a2bf34b848b1aadb55dfdb6e20db3c30da933
Anubys <kat.entwickler@gmail.com>
2020-05-05 00:12:31 +0200

    Create Project: FirebaseConnectionDemo  + connection to Firebase  + add ButterKnife

---
+++ Java/Examples/FirebaseConnectionDemo/app/src/main/java/com/anubys/example/firebaseconnectiondemo/MainActivity.java
@@ -0,0 +1,58 @@
+package com.anubys.example.firebaseconnectiondemo;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/** @Autor Created by Anubys on the 29.03.2020 */
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Nullable
+    @BindView(R.id.tv_message)
+    TextView textViewMessage;
+
+    private Unbinder unbinder;
+
+
+    //* ************************************************ *
+    //*               L I F E - C Y C L E                *
+    //* ************************************************ *
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "TAG - MainActivity - onCreate()");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        unbinder = ButterKnife.bind(this);
+
+        // Datenbankverbindung aufbauen
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        // Ein Objekt erstellen
+        DatabaseReference databaseReference = firebaseDatabase.getReference("message");
+        // Ein Kindelement von Eltern erstellen und einen Wert setzten
+        databaseReference.child("msg1").setValue("Hallo2!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "TAG - MainActivity - onDestroy()");
+        super.onDestroy();
+
+        if (unbinder != null) {
+            unbinder.unbind();
+        }
+    }
+}
