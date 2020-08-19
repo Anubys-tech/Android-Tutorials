590995f37f3a2f530378640fe4ef5f5315b10a22
Anubys <kat.entwickler@gmail.com>
2020-08-19 15:22:24 +0200

    Project: ViewBindingFragmentDemo  + add TextView in fragment_home.xml

--- Kotlin/Examples/ViewBindingFragmentDemo/app/src/main/java/com/anubys/example/viewbindingfragmentdemo/ui/home/HomeViewModel.kt
+++ Kotlin/Examples/ViewBindingFragmentDemo/app/src/main/java/com/anubys/example/viewbindingfragmentdemo/ui/home/HomeViewModel.kt
@@ -12,5 +12,5 @@ class HomeViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
-    val text: LiveData<String> = _text
+    var text: LiveData<String> = _text
 }
